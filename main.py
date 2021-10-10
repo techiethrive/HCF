513a0a0380436242fc33b5f6ddcab69cd8cb431d
Aman Kumar <70050351+theamankumar@users.noreply.github.com>
2021-10-10 14:01:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+a = int(input('Enter First Integer: '))
+b = int(input('Enter Second Integer: '))
+num = a and b
+for i in range(num,1,-1):
+    if a % i == 0 and b % i == 0 :
+        print(f"HCF of {a} and {b} is {i}")
+        break
+else:
+    print(f'HCF of {a} and {b} is 1, So They are Co-Prime.')
